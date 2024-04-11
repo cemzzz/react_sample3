dd1e31b05510cd259298692c772e3f57398de0be
cemzzz <koreaok01@gmail.com>
2024-04-11 18:12:08 +0900

    mySql 최신화

--- src/server/SNS_SQL.sql
+++ src/server/SNS_SQL.sql
@@ -34,7 +34,12 @@ INSERT INTO `tbl_sns_board` (`boardNo`, `userId`, `content`, `cdatetime`) VALUES
 	(22, 'user3', '테스트 등록', '2024-04-11 11:36:00'),
 	(23, 'user3', '한 번 더 테스트', '2024-04-11 11:50:18'),
 	(24, 'user1', '이것도 등록', '2024-04-11 11:55:20'),
-	(25, 'user3', '채소 게시글 등록 테스트', '2024-04-11 13:17:28');
+	(25, 'user3', '채소 게시글 등록 테스트', '2024-04-11 13:17:28'),
+	(27, 'user2', '생막창과 삼겹살이 맛있는 #불막열삼합정점... 더 보기', '2024-04-11 15:36:40'),
+	(30, 'user2', '귀여운 우사기 덕질~~~~', '2024-04-11 15:46:27'),
+	(31, 'mimi', '그렇다면 난 치이카와 덕질~!', '2024-04-11 16:15:02'),
+	(32, 'user1', '난 정말 예뻐~~ #미스코리아 대상', '2024-04-11 16:19:35'),
+	(33, 'bibi', '다이어트 156일째.... #바프 #다이어트 #헬스', '2024-04-11 16:41:58');
 
 -- 테이블 test.tbl_sns_images 구조 내보내기
 CREATE TABLE IF NOT EXISTS `tbl_sns_images` (
@@ -52,7 +57,15 @@ INSERT INTO `tbl_sns_images` (`fileNo`, `boardNo`, `filePath`, `fileName`, `file
 	(11, 22, 'img/', '240411113600_img1.jpg', 'img1.jpg'),
 	(12, 23, 'img/', '240411115018_vegetable4.jpg', 'vegetable4.jpg'),
 	(13, 24, 'img/', '240411115520_img.jpg', 'img.jpg'),
-	(14, 25, 'img/', '240411131728_vegetable3.jpg', 'vegetable3.jpg');
+	(14, 25, 'img/', '240411131728_vegetable3.jpg', 'vegetable3.jpg'),
+	(15, 26, 'img/', '240411153426_곱창.jpg', '곱창.jpg'),
+	(16, 27, 'img/', '240411153640_gogogo.jpg', 'gogogo.jpg'),
+	(17, 28, 'img/', '240411154257_USAUSA.jpg', 'USAUSA.jpg'),
+	(18, 29, 'img/', '240411154320_USAUSA.jpg', 'USAUSA.jpg'),
+	(19, 30, 'img/', '240411154627_USAUSA.jpg', 'USAUSA.jpg'),
+	(20, 31, 'img/', '240411161502_hachi.png', 'hachi.png'),
+	(21, 32, 'img/', '240411161935_beauty.jpg', 'beauty.jpg'),
+	(22, 33, 'img/', '240411164158_manyfood.jpg', 'manyfood.jpg');
 
 -- 테이블 test.tbl_sns_user 구조 내보내기
 CREATE TABLE IF NOT EXISTS `tbl_sns_user` (
@@ -68,6 +81,8 @@ CREATE TABLE IF NOT EXISTS `tbl_sns_user` (
 
 -- 테이블 데이터 test.tbl_sns_user:~3 rows (대략적) 내보내기
 INSERT INTO `tbl_sns_user` (`userid`, `userPwd`, `username`, `follower`, `following`, `profile`, `profileImage`) VALUES
+	('bibi', '123456', '', 0, 0, '세계 최강의 사나이가 될 몸이시다', 'https://wimg.mk.co.kr/news/cms/202309/30/news-p.v1.20230930.e202dea5b30244fdb88348392c31289a_P1.jpeg'),
+	('mimi', '1234', '예삐', 0, 0, '안녕하세요 예삐입니다.', 'https://i.namu.wiki/i/2xZhQPaKlDgvkrL_bkhgo4UpJswzK8Y59paeOh7GO3hcKBXTKm_lkDxBcePbMAZzT6A98h6YB0CLV1tanvz6_w.webp'),
 	('user1', '1234', '마동석', 100, 150, '안녕하세요! 홍길동입니다. 만나서 반가워요.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq8dsmjbmBWcREmiATXIAbJx-E2oB7lf0K1C3UuxB9_A&s'),
 	('user2', '1234', '은가누', 200, 300, '안녕하세요, 사용자 2입니다. 제 프로필에 오신 것을 환영합니다.', 'https://img.wowtv.co.kr/wowtv_news/dnrs/20230323/2023032308145802122d3244b4fed182172185139.jpg'),
 	('user3', '1234', '장원영', 50, 75, '안녕하세요! 사용자 3입니다. 연락해 주세요.', 'https://i.namu.wiki/i/_hZOlTayAgj4o2_poQRibNYekqbFl8YMTKW-sgiLguSDL47L_Zjcx5L2zaIwf7ZSasImsryUAHyjKM9-DqvlGw.webp');
